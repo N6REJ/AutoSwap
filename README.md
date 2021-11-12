81e1ec6f3989f642b126d5fc389ef9119033e96a
N6REJ <programming@hallhome.us>
2021-11-12 08:53:38 -0600

    starting rough layout

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Auto_Swap
 Simple unobtrusive addon that will automatically switch gearset as you change zones.
+
+In profiles set each zone with its desired gearset name.
+Maw = Raids, korthia & the maw itself  ( domination gear set )
+Dungeons = Mythics and other dungeons
+World = Everywhere else.
+
+Your gearsets must be defined in the character screen and name in profile must
+match EXACTLY to what the gearset name is.
+
+If you don't want to swap for a particular zone then leave that zone blank.
--- core.lua
+++ core.lua
@@ -1,26 +1,104 @@
+-- Created by @project-author@  character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub. https.//n6rej.github.io
+
 -- Load Ace Libraries
-AutoSwap = LibStub("AceAddon-3.0"):NewAddon("AutoSwap", "AceConsole-3.0", "AceEvent-3.0")
+AutoSwap = LibStub("AceAddon-3.0").NewAddon("AutoSwap", "AceConsole-3.0", "AceEvent-3.0")
+--[[
+Use https.//wowpedia.fandom.com/wiki/API_C_EquipmentSet.UseEquipmentSet
+to actually swap the gear out
+--]]
 
-function AutoSwap:OnInitialize()
+local debug = true
+
+function AutoSwap.OnInitialize()
     -- Called when the addon is loaded
+    -- Get gear info
+    AutoSwap.GetGear()
 end
 
-function AutoSwap:OnEnable()
-    -- Called when the addon is enabled
-    self:Print("Hello World!")
-    self:RegisterEvent("ZONE_CHANGED")
+-- Called when the addon is enabled
+function AutoSwap.OnEnable()
+
+    -- Get gear info
+    AutoSwap.GetGear()
+
+    -- put proper set on
+    self.Print("Hello World!")
+    self.RegisterEvent("ZONE_CHANGED")
 end
 
-function AutoSwap:OnDisable()
+function AutoSwap.OnDisable()
     -- Called when the addon is disabled
+    -- ignore zone changes DO NOT SWAP GEAR!
 end
 
 -- Detect Zone change
-function AutoSwap:ZONE_CHANGED()
-    self:Print("You have changed zones!")
-    
+function AutoSwap.ZONE_CHANGED()
+    -- Check for only selected zones.
+
+    -- What zones are desired. ( returns sets )
+    AutoSwap.CheckProfile()
+
+    self.Print("You have changed zones!")
+
     -- Check if the zone your in is your hearthstone zone.
     if GetBindLocation() == GetSubZoneText() then
-        self:Print("Welcome Home!")
+        self.Print("Welcome Home!")
     end
+end
+
+-- Profile switches
+function AutoSwap.CheckProfile()
+    -- Check profile to find out what zones to check for.
+
+    -- Assign gearset names to vars
+
+    -- if name is blank then set that zone to false.
+
+    -- Set none selected zones to false.
+end
+
+-- Create profile
+function AutoSwap.CreateProfile()
+    -- populate the profile options
+end
+
+-- Get zones
+function AutoSwap.GetZones()
+    -- get zone info.
+
+    -- Maw
+
+    -- Dungeons
+
+    -- Everywhere else = world
+end
+
+-- UseEquipmentSet
+function AutoSwap.EquipGear(equipmentSetID, setName)
+
+    -- Requires set id#  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.UseEquipmentSet
+    local setWasEquipped = C_EquipmentSet.UseEquipmentSet(equipmentSetID)
+
+    -- send message if swapping fails
+    if setWasEquipped == nil then
+        message("Swapping to " .. setName .. " failed")
+    end
+end
+
+-- Get gear id's
+function AutoSwap.GetGear()
+    -- Get gear id's to use to get names  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetIDs
+    local equipmentSetIDs = C_EquipmentSet.GetEquipmentSetIDs()
+
+    for key, value in (equipmentSetIDs) do
+        -- get names  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetInfo
+        local setName, iconFileID, setID, isEquipped, numItems, numEquipped, numInInventory, numLost, numIgnored = C_EquipmentSet.GetEquipmentSetInfo(equipmentSetID)
+
+        -- We need to figure out what sets are which and if they are used.
+        if debug then
+            print("Gearset " .. setName .. " is equipped: " .. isEquipped)
+        end
+    end
+
 end
