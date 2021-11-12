a22069fe147b9ed7789e77f7fd87d5a9bffa4037
N6REJ <programming@hallhome.us>
2021-11-12 11:29:07 -0600

    runs w/o errors

--- core.lua
+++ core.lua
@@ -2,9 +2,9 @@
 -- Source on GitHub. https.//n6rej.github.io
 
 -- Load Ace Libraries
--- Addon handler: https://www.wowace.com/projects/ace3/pages/api/ace-addon-3-0
--- Slash commands: https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0
--- Event handler: https://www.wowace.com/projects/ace3/pages/api/ace-event-3-0
+-- Addon handler. https.//www.wowace.com/projects/ace3/pages/api/ace-addon-3-0
+-- Slash commands. https.//www.wowace.com/projects/ace3/pages/api/ace-console-3-0
+-- Event handler. https.//www.wowace.com/projects/ace3/pages/api/ace-event-3-0
 
 AutoSwap = LibStub("AceAddon-3.0"):NewAddon("AutoSwap", "AceConsole-3.0", "AceEvent-3.0")
 --[[
@@ -15,30 +15,30 @@ to actually swap the gear out
 local debug = true
 
 -- Called when the addon is loaded
-function AutoSwap.OnInitialize()
+function AutoSwap:OnInitialize()
     -- do init tasks here, like loading the Saved Variables,
     -- or setting up slash commands.
 
     -- Get gear info
-    AutoSwap.GetGear()
+    AutoSwap:GetGear()
 end
 
 -- Called when the addon is enabled
-function AutoSwap.OnEnable()
+function AutoSwap:OnEnable()
     -- Do more initialization here, that really enables the use of your addon.
     -- Register Events, Hook functions, Create Frames, Get information from
     -- the game that wasn't available in OnInitialize
 
     -- Get gear info
-    AutoSwap.GetGear()
+    AutoSwap:GetGear()
 
     -- put proper set on
-    self.Print("Hello World!")
-    self.RegisterEvent("ZONE_CHANGED")
+    self:Print("Hello World!")
+    self:RegisterEvent("ZONE_CHANGED")
 end
 
 -- Called when the addon is disabled
-function AutoSwap.OnDisable()
+function AutoSwap:OnDisable()
     -- Unhook, Unregister Events, Hide frames that you created.
     -- You would probably only use an OnDisable if you want to
     -- build a "standby" mode, or be able to toggle modules on/off.
@@ -47,11 +47,11 @@ function AutoSwap.OnDisable()
 end
 
 -- Detect Zone change
-function AutoSwap.ZONE_CHANGED()
+function AutoSwap:ZONE_CHANGED()
     -- Check for only selected zones.
 
     -- What zones are desired. ( returns sets )
-    AutoSwap.CheckProfile()
+    AutoSwap:CheckProfile()
 
     self.Print("You have changed zones!")
 
@@ -62,7 +62,7 @@ function AutoSwap.ZONE_CHANGED()
 end
 
 -- Profile switches
-function AutoSwap.CheckProfile()
+function AutoSwap:CheckProfile()
     -- Check profile to find out what zones to check for.
 
     -- Assign gearset names to vars
@@ -73,12 +73,12 @@ function AutoSwap.CheckProfile()
 end
 
 -- Create profile
-function AutoSwap.CreateProfile()
+function AutoSwap:CreateProfile()
     -- populate the profile options
 end
 
 -- Get zones
-function AutoSwap.GetZones()
+function AutoSwap:GetZones()
     -- get zone info.
 
     -- Maw
@@ -89,9 +89,9 @@ function AutoSwap.GetZones()
 end
 
 -- UseEquipmentSet
-function AutoSwap.EquipGear(equipmentSetID, setName)
+function AutoSwap:EquipGear(equipmentSetID, setName)
 
-    -- Requires set id#  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.UseEquipmentSet
+    -- Requires set id#  https.//wowpedia.fandom.com/wiki/API_C_EquipmentSet.UseEquipmentSet
     local setWasEquipped = C_EquipmentSet.UseEquipmentSet(equipmentSetID)
 
     -- send message if swapping fails
@@ -101,18 +101,18 @@ function AutoSwap.EquipGear(equipmentSetID, setName)
 end
 
 -- Get gear id's
-function AutoSwap.GetGear()
-    -- Get gear id's to use to get names  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetIDs
+function AutoSwap:GetGear()
+    -- Get gear id's to use to get names  https.//wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetIDs
     local equipmentSetIDs = C_EquipmentSet.GetEquipmentSetIDs()
 
     for key, equipmentSetID in ipairs(equipmentSetIDs) do
-        -- get names  https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetInfo
+        -- get names  https.//wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetInfo
         local setName, iconFileID, setID, isEquipped, numItems, numEquipped, numInInventory, numLost, numIgnored = C_EquipmentSet.GetEquipmentSetInfo(equipmentSetID)
 
 
         -- We need to figure out what sets are which and if they are used.
         if debug then
-            print("Gearset " .. setName .. " is equipped: " , isEquipped)
+            print("Gearset " .. setName .. " is equipped. " , isEquipped)
         end
     end
 
